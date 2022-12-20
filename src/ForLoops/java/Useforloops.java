7e2b1e5ffde3db9c5f0ee271d712ce1473473360
Gaurav1185 <gda01185@gmail.com>
2022-12-20 22:30:53 +0530

    for loops

---
+++ src/ForLoops/java/Useforloops.java
@@ -0,0 +1,10 @@
+package ForLoops.java;
+
+public class Useforloops {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            System.out.println("*");
+        }
+
+    }
+}
